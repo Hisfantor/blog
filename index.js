de3f2b3780cb5e6a475e98feb290b394daf5c1fd
Hisfantor <i.nici@icloud.com>
2018-12-26 16:35:34 +0100

    header test

---
+++ index.js
@@ -0,0 +1,63 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+        $('nav').addClass('fixed-header');     
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+    }
+});
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up');
+        }
+    }
+    
+    lastScrollTop = st;
+}
+
+//------------------------
+
+function changeClass(){
+    document.getElementById("contentbox").classList.toggle('content');    
+}
+
+   /* window.onload = function(){
+        document.getElementById("button").addEventListener( 'click', changeClass());
+    }*/
+
+$(document).ready(function() {
+     
+            
+            // Animate the scroll to top
+            $('.go-top').click(function(event) {
+                event.preventDefault();
+                
+                $('html, body').animate({scrollTop: 0}, 300);
+            })
+        });
